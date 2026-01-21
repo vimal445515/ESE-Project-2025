4ae2c06c8f0c4c8a2f5ff15ccc4bd4616230fa48
vimal445515 <vv8280203@gmail.com>
2026-01-21 18:35:23 +0530

    add filter based on dates in backend and add revenue feild in ui

--- Controllers/salesReportControllers.js
+++ Controllers/salesReportControllers.js
@@ -1,7 +1,10 @@
 import salesReportService from "../Service/salesReportService.js"
 
 const loadSalesReportPage = async(req,res)=>{
-    const salesReport = await salesReportService.getSalesReport()
+    const startDate = req.query.startDate?new Date( req.query.startDate):null
+    const endDate = req.query.endDate?new Date( req.query.endDate):null
+    console.log(startDate,endDate)
+    const salesReport = await salesReportService.getSalesReport(startDate,endDate)
     console.log(salesReport)
     res.status(200).render('Admin/SalesReportPage')
 }
--- Service/salesReportService.js
+++ Service/salesReportService.js
@@ -1,16 +1,25 @@
 import mongoose from 'mongoose'
 import orderModel from '../Models/orderSchema.js'
 
-const getSalesReport = ()=>{
-    const salesReport = orderModel.aggregate([
-        {$match:{orderStatus:'delivered'}},
+const getSalesReport = (startDate,endDate)=>{
+    
+    const pipeline = []
+    if(startDate){
+       
+         pipeline.push({$match:{createdAt:{$gte:startDate,$lte:endDate}}})
+    }
+    
+    pipeline.push({$match:{orderStatus:'delivered'}},
         {$group:{_id:null,
             totalOrder:{$sum:1},
             overalOrderAmount:{$sum:'$pricing.subTotal'},
             overalDescountAmount:{$sum:'$pricing.discount'},
             netSales:{$sum:'$pricing.totalAmount'}
-        }}
-    ])
+        }})
+
+       
+
+    const salesReport = orderModel.aggregate(pipeline)
 
     return salesReport;
 }
