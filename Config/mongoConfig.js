1484eb50847f2e9faf1a18f7bd8f8376c3aca2be
VIMAL V <vv8280203@gmail.com>
2026-02-27 12:01:33 +0530

    Update mongoConfig.js

--- Config/mongoConfig.js
+++ Config/mongoConfig.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL,{
+      ssl: true,
+      tls: true,
+      tlsAllowInvalidCertificates: false,
+    });
     console.log("Database connected successfully");
   } catch (error) {
     console.log(error);
