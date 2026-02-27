340036b6cf3a383c36b3ee78d1b518d8e80d472f
VIMAL V <vv8280203@gmail.com>
2026-02-27 11:06:51 +0530

    Update mongoConfig.js

--- Config/mongoConfig.js
+++ Config/mongoConfig.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
-    console.log(process.env.MONGODB_URL)
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected successfully");
   } catch (error) {
