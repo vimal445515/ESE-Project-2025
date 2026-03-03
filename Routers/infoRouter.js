bb5282ea92d06899618038c3b93eda14b6fe7600
vimal445515 <vv8280203@gmail.com>
2026-03-03 15:12:09 +0530

    Add: after login same state as  befor login for product details page.

--- Routers/infoRouter.js
+++ Routers/infoRouter.js
@@ -1,22 +1,45 @@
 import infoController from "../Controllers/infoController.js";
-import {Router} from "express"
+import { Router } from "express";
 
 const router = new Router();
 
-router.get('/about',(req,res)=>{
-    res.status(200).render('User/aboutPage',{userName:req.session?.userName||null,profile:req.session?.profile||null});
-})
-router.get('/404Page',(req,res)=>{
-    res.status(404).render('User/404Page',{userName:req.session?.userName||null,profile:req.session?.profile||null})
-})
+router.get("/about", (req, res) => {
+  if (req.session?.productId) {
+    delete req.session.productId;
+    delete req.session.variantId;
+  }
+  res
+    .status(200)
+    .render("User/aboutPage", {
+      userName: req.session?.userName || null,
+      profile: req.session?.profile || null,
+    });
+});
+router.get("/404Page", (req, res) => {
+  res
+    .status(404)
+    .render("User/404Page", {
+      userName: req.session?.userName || null,
+      profile: req.session?.profile || null,
+    });
+});
 
-router.get('/contact',(req,res)=>{
-    res.status(200).render('User/contactPage',{userName:req.session?.userName||null,profile:req.session?.profile||null})
-})
+router.get("/contact", (req, res) => {
+  if (req.session?.productId) {
+    delete req.session.productId;
+    delete req.session.variantId;
+  }
+  res
+    .status(200)
+    .render("User/contactPage", {
+      userName: req.session?.userName || null,
+      profile: req.session?.profile || null,
+    });
+});
 
-router.get('/500Error',(req,res)=>{
-    res.status(500).render('User/500Page')
-})
+router.get("/500Error", (req, res) => {
+  res.status(500).render("User/500Page");
+});
 
-router.post('/contact',infoController.sendMessage)
-export default router
+router.post("/contact", infoController.sendMessage);
+export default router;
